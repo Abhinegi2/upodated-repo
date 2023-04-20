cdb20fc888346249f26c4a7596fdedb76fb55ff8
anuj kumar <anuj.kumar@coloredcow.in>
2023-04-20 17:13:04 +0530

    rm console

--- src/app/main/head-masters/head-master.component.ts
+++ src/app/main/head-masters/head-master.component.ts
@@ -144,7 +144,6 @@ export class HeadMasterComponent extends BaseListComponent<HeadMasterModel> impl
     };
 
     this.headMasterService.GetAllByCriteria(hmParams).subscribe(response => {
-      console.log(response);
       this.displayedColumns = [
         'SchoolName',
         'FullName',
