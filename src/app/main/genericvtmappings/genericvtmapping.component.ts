e9fcdc586aa15ea5cc0b0ddd22fe1e8e7a4053f9
anuj kumar <anuj.kumar@coloredcow.in>
2023-05-22 10:56:08 +0530

    Provide Filters In SSJ,VTACS And GenericVTMapping

--- src/app/main/genericvtmappings/genericvtmapping.component.ts
+++ src/app/main/genericvtmappings/genericvtmapping.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit, NgZone, ViewEncapsulation } from '@angular/core';
 import { CommonService } from 'app/services/common.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { DropdownModel } from 'app/models/dropdown.model';
 import { GenericVTMappingModel } from './genericvtmapping.model';
 import { GenericVTMappingService } from './genericvtmapping.service';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { BaseListComponent } from 'app/common/base-list/base.list.component';
 import { fuseAnimations } from '@fuse/animations';
 import { DialogService } from 'app/common/confirm-dialog/dialog.service';
@@ -17,18 +19,67 @@ import { DialogService } from 'app/common/confirm-dialog/dialog.service';
 })
 
 export class GenericVTMappingComponent extends BaseListComponent<GenericVTMappingModel> implements OnInit {
+
+  genericvtmappingFilterForm: FormGroup;
+  UserId: string;
+  userList: DropdownModel[];
+  userFilterList: any;
+  gvtList: [DropdownModel];
+  vtpList: [DropdownModel];
+  vcList: [DropdownModel];
+  vtList: [DropdownModel];
+
   constructor(public commonService: CommonService,
     public router: Router,
     public routeParams: ActivatedRoute,
     public snackBar: MatSnackBar,
+    public formBuilder: FormBuilder,
     public zone: NgZone,
     private dialogService: DialogService,
     private genericvtmappingService: GenericVTMappingService) {
     super(commonService, router, routeParams, snackBar, zone);
+
+    this.genericvtmappingFilterForm = this.creategenericvtmappingFilterForm();  
   }
 
   ngOnInit(): void {
-    this.genericvtmappingService.GetAllByCriteria(this.SearchBy).subscribe(response => {
+    this.genericvtmappingService.getGenericVTMapping(this.UserModel).subscribe(results => {
+console.log(results);
+      if (results[1].Success) {
+        this.vtpList = results[1].Results;
+      }
+
+      if (results[2].Success) {
+        this.vcList = results[2].Results;
+      }
+
+      if (results[3].Success) {
+        this.vtList = results[3].Results;
+      }
+      this.onLoadGenericVTMappingByCriteria();
+      
+    });
+  }
+
+  onLoadGenericVTMappingByCriteria(): void {
+    this.IsLoading = true;
+console.log(this.genericvtmappingFilterForm.controls["VTPId"].value)
+console.log(this.genericvtmappingFilterForm.controls["VTId"].value)
+console.log(this.genericvtmappingFilterForm.controls["VCId"].value)
+console.log(this.UserModel.UserTypeId)
+    let genericvtParams: any = {
+      UserTypeId: this.UserModel.UserTypeId,
+      VTPId: this.genericvtmappingFilterForm.controls["VTPId"].value,
+      VCId: this.genericvtmappingFilterForm.controls["VCId"].value,
+      VTId: this.genericvtmappingFilterForm.controls["VTId"].value,
+     Status: this.genericvtmappingFilterForm.controls["Status"].value,
+     CharBy: null,
+     PageIndex: this.SearchBy.PageIndex,
+     PageSize: this.SearchBy.PageSize
+    };
+
+    this.genericvtmappingService.GetAllByCriteria(genericvtParams).subscribe(response => {
+      console.log(response);
       this.displayedColumns = [
         // 'UserType', 'UserName',
         'VTPShortName',
@@ -48,6 +99,7 @@ export class GenericVTMappingComponent extends BaseListComponent<GenericVTMappin
       this.tableDataSource.sort = this.ListSort;
       this.tableDataSource.paginator = this.ListPaginator;
       this.tableDataSource.filteredData = this.tableDataSource.data;
+      this.SearchBy.TotalResults = response.TotalResults;
 
       this.zone.run(() => {
         if (this.tableDataSource.data.length == 0) {
@@ -58,6 +110,21 @@ export class GenericVTMappingComponent extends BaseListComponent<GenericVTMappin
     }, error => {
       console.log(error);
     });
+  }
+
+  onLoadGenericVTMappingByFilters(): any {
+    this.SearchBy.PageIndex = 0;
+    this.onLoadGenericVTMappingByCriteria();
+  }
+
+  resetFilters(): void {
+
+    this.genericvtmappingFilterForm.reset();
+    this.genericvtmappingFilterForm.get('AcademicYearId').setValue(this.AcademicYearId);
+
+    // this.vcList = [];
+    // this.filteredVCItems = [];
+    this.onLoadGenericVTMappingByCriteria();
   }
 
   onDeleteGenericVTMapping(genericvtmappingId: string) {
@@ -86,4 +153,14 @@ export class GenericVTMappingComponent extends BaseListComponent<GenericVTMappin
         }
       });
   }
+
+    //Create VocationalTrainingProviderProvider form and returns {FormGroup}
+    creategenericvtmappingFilterForm(): FormGroup {
+      return this.formBuilder.group({
+        VTPId: new FormControl(),
+        VCId: new FormControl(),
+        VTId: new FormControl(),
+        Status: new FormControl()
+      });
+    }
 }
