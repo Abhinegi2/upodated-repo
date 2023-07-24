de262f31e11f1740c77d5fd27174e3b333c324d7
aquibmoin786 <aquibmoin786@gmail.com>
2023-07-24 13:29:59 +0530

    final PR

--- src/app/main/broadcast-messages/i18n/en.ts
+++ src/app/main/broadcast-messages/i18n/en.ts
@@ -2,7 +2,7 @@ export const locale = {
     lang: 'en',
     data: {
         'HEADMASTER': {
-            'HEAD_MASTERS': 'HOS',
+            'HEAD_MASTERS': 'HEAD MASTERS',
             'ADD_NEW_HEAD_MASTER': 'ADD',
             'SEARCH_FOR_A_HEAD_MASTER': 'Search for a head masters',
         }
