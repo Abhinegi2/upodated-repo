f85f3374b77cf1c78c8fbc19be639818e01cbf99
anuj kumar <anuj.kumar@coloredcow.in>
2023-08-05 07:44:05 +0530

    revert the changes of genericvtmapping

--- src/app/main/genericvtmappings/genericvtmapping.component.ts
+++ src/app/main/genericvtmappings/genericvtmapping.component.ts
@@ -2,10 +2,8 @@ import { Component, OnInit, NgZone, ViewEncapsulation } from '@angular/core';
 import { CommonService } from 'app/services/common.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { DropdownModel } from 'app/models/dropdown.model';
 import { GenericVTMappingModel } from './genericvtmapping.model';
 import { GenericVTMappingService } from './genericvtmapping.service';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { BaseListComponent } from 'app/common/base-list/base.list.component';
 import { fuseAnimations } from '@fuse/animations';
 import { DialogService } from 'app/common/confirm-dialog/dialog.service';
@@ -19,56 +17,20 @@ import { DialogService } from 'app/common/confirm-dialog/dialog.service';
 })
 
 export class GenericVTMappingComponent extends BaseListComponent<GenericVTMappingModel> implements OnInit {
-
-  genericvtmappingFilterForm: FormGroup;
-  UserId: string;
-  userList: DropdownModel[];
-  userFilterList: any;
-  vtpList: [DropdownModel];
-
   constructor(public commonService: CommonService,
     public router: Router,
     public routeParams: ActivatedRoute,
     public snackBar: MatSnackBar,
-    public formBuilder: FormBuilder,
     public zone: NgZone,
     private dialogService: DialogService,
     private genericvtmappingService: GenericVTMappingService) {
     super(commonService, router, routeParams, snackBar, zone);
-
-    this.genericvtmappingFilterForm = this.creategenericvtmappingFilterForm();
   }
 
   ngOnInit(): void {
-    this.genericvtmappingService.getGenericVTMapping(this.UserModel).subscribe(results => {
-
-      if (results[1].Success) {
-        this.vtpList = results[1].Results;
-      }
-
-      this.onLoadGenericVTMappingByCriteria();
-
-    });
-  }
-
-  onLoadGenericVTMappingByCriteria(): void {
-    this.IsLoading = true;
-
-    let genericvtParams: any = {
-      UserTypeId: this.UserModel.UserTypeId,
-      GVTId: this.genericvtmappingFilterForm.controls["GVTId"].value,
-      VTPId: this.genericvtmappingFilterForm.controls["VTPId"].value,
-      VCId: this.genericvtmappingFilterForm.controls["VCId"].value,
-      VTId: this.genericvtmappingFilterForm.controls["VTId"].value,
-      Status: this.genericvtmappingFilterForm.controls["Status"].value,
-      CharBy: null,
-      PageIndex: this.SearchBy.PageIndex,
-      PageSize: this.SearchBy.PageSize
-    };
-
-    this.genericvtmappingService.GetAllByCriteria(genericvtParams).subscribe(response => {
-      console.log(response);
+    this.genericvtmappingService.GetAllByCriteria(this.SearchBy).subscribe(response => {
       this.displayedColumns = [
+        // 'UserType', 'UserName',
         'VTPShortName',
         'VCFullName',
         'VTFullName',
@@ -81,7 +43,7 @@ export class GenericVTMappingComponent extends BaseListComponent<GenericVTMappin
         'CreatedBy', 
         'UpdatedBy', 
         'DateOfRemoval',
-        'IsActive',
+        'IsActive', 
         'Actions'
       ];
 
@@ -89,7 +51,6 @@ export class GenericVTMappingComponent extends BaseListComponent<GenericVTMappin
       this.tableDataSource.sort = this.ListSort;
       this.tableDataSource.paginator = this.ListPaginator;
       this.tableDataSource.filteredData = this.tableDataSource.data;
-      this.SearchBy.TotalResults = response.TotalResults;
 
       this.zone.run(() => {
         if (this.tableDataSource.data.length == 0) {
@@ -100,21 +61,6 @@ export class GenericVTMappingComponent extends BaseListComponent<GenericVTMappin
     }, error => {
       console.log(error);
     });
-  }
-
-  onLoadGenericVTMappingByFilters(): any {
-    this.SearchBy.PageIndex = 0;
-    this.onLoadGenericVTMappingByCriteria();
-  }
-
-  resetFilters(): void {
-
-    this.genericvtmappingFilterForm.reset();
-    this.genericvtmappingFilterForm.get('AcademicYearId').setValue(this.AcademicYearId);
-
-    // this.vcList = [];
-    // this.filteredVCItems = [];
-    this.onLoadGenericVTMappingByCriteria();
   }
 
   onDeleteGenericVTMapping(genericvtmappingId: string) {
@@ -142,12 +88,5 @@ export class GenericVTMappingComponent extends BaseListComponent<GenericVTMappin
           this.ngOnInit();
         }
       });
-  }
-
-  creategenericvtmappingFilterForm(): FormGroup {
-    return this.formBuilder.group({
-      VTPId: new FormControl(),
-      Status: new FormControl()
-    });
   }
 }
