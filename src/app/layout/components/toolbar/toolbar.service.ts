d8bd5b6c86b5b2ee9cf5bd080d9afb2bb6c7692c
anuj kumar <kanuj101890@gmail.com>
2024-01-05 03:02:27 +0530

    updatehe file

--- src/app/layout/components/toolbar/toolbar.service.ts
+++ src/app/layout/components/toolbar/toolbar.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from "@angular/core";
-import { forkJoin, Observable } from "rxjs";
-import { retry, catchError, tap } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { catchError } from "rxjs/operators";
 import { BaseService } from 'app/services/base.service';
-import { UserModel } from 'app/models/user.model';
-
 
 @Injectable({
   providedIn: 'root'
@@ -13,15 +11,14 @@ export class ToolbarService {
   constructor(private http: BaseService) { }
 
   saveUserLocation(userId: number, latitude: number, longitude: number): Observable<any> {
-    const url = 'your_backend_api_endpoint'; // Replace with your actual backend API endpoint
+    const url = 'LighthouseServices/Toolbar/CreateToolbar';
 
     const payload = {
-      userId: userId,
-      latitude: latitude,
-      longitude: longitude
+      UserId: userId,
+      Latitude: latitude,
+      Longitude: longitude
     };
 
-    // Assuming you have a method in BaseService for making HTTP requests, adjust it accordingly
     return this.http.post(url, payload).pipe(
       catchError(error => {
         console.error('Error saving user location:', error);
