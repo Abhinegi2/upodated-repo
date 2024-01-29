4b34008e240b7f2e34be09850c68e80fced9c6e9
NirmalNegi07 <www.NirmalNegi2001@gmail.com>
2024-01-29 21:12:19 +0530

    Submission Date field in the Course Material Reports

--- src/app/reports/course-material-status/course-material-status.component.ts
+++ src/app/reports/course-material-status/course-material-status.component.ts
@@ -216,8 +216,7 @@ export class CourseMaterialStatusComponent extends BaseListComponent<CourseMater
     reportParams.DistrictId = (reportParams.DistrictId != null && reportParams.DistrictId.length > 0) ? reportParams.DistrictId.toString() : null;
 
     this.reportService.GetCourseMaterialStatusReportsByCriteria(reportParams).subscribe(response => {
-      this.displayedColumns = ['SrNo', 'AcademicYear', 'SchoolAllottedYear', 'PhaseName', 'VTPName', 'VCName', 'VCMobile', 'VCEmail', 'VTName', 'VTMobile', 'VTEmail', 'VTDateOfJoining', 'HMName', 'HMMobile', 'HMEmail', 'SchoolManagement', 'DivisionName', 'DistrictName', 'BlockName', 'UDISE', 'SchoolName', 'SectorName', 'JobRoleName', 'ClassName', 'CourceMaterialAvailability', 'DateOfReceipt', 'MonthYear', 'CourceMaterialStatus'];
-
+      this.displayedColumns = ['SrNo', 'AcademicYear', 'SchoolAllottedYear', 'PhaseName', 'VTPName', 'VCName', 'VCMobile', 'VCEmail', 'VTName', 'VTMobile', 'VTEmail', 'VTDateOfJoining', 'HMName', 'HMMobile', 'HMEmail', 'SchoolManagement', 'DivisionName', 'DistrictName', 'BlockName', 'UDISE', 'SchoolName', 'SectorName', 'JobRoleName', 'ClassName', 'CourceMaterialAvailability', 'DateOfReceipt','SubmissionDate', 'MonthYear', 'CourceMaterialStatus'];
       this.tableDataSource.data = response.Results;
       this.tableDataSource.sort = this.ListSort;
       this.tableDataSource.paginator = this.ListPaginator;
